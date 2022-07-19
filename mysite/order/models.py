9896446a3055fd241e930edb4e0e215eb5e1564c
Ignas Navickas <92112858+TGIgnas@users.noreply.github.com>
2022-07-20 01:02:07 +0300

    Added shopcart views fixed some templates

--- mysite/order/models.py
+++ mysite/order/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from product.models import Product
 from django.forms import ModelForm
-import uuid
 from django.db import models
 
 
@@ -39,7 +38,7 @@ class Order(models.Model):
         ('Canceled', 'Canceled'),
     )
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=5, editable=False )
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     phone = models.CharField(blank=True, max_length=20)
--- mysite/order/urls.py
+++ mysite/order/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('order/', views.order, name='order'),
+    path('addtoshopcart/<int:id>', views.addtoshopcart, name='addtoshopcart'),
+    path('deletefromcart/<int:id>', views.deletefromcart, name='orddeletefromcarter'),
+    path('orderproduct/', views.orderproduct, name='orderproduct'),
 
 ]
